008890ff86d85738cdf90d8f1ec70a6e9d09e4aa
psd9988 <psd9988@gmail.com>
2022-09-14 18:37:03 +0530

    Myntra Team H1

---
+++ script.js
@@ -0,0 +1,168 @@
+// After Header Part of the Body Starts Here:-
+
+
+let imagesRow1 = [
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/6bf8a883-6a9d-4cb6-b330-38325a4fc3951662101284444-Sale-Is-Live_02.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/9d29ad74-7a45-4374-b13d-c57bff814b3c1662101284451-Sale-Is-Live_03.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/9702c7ae-424b-4231-83ab-d34d4ef46c5f1662101284458-Sale-Is-Live_04.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/ec4ae20d-1b70-409e-9e98-30c55951349e1662101284465-Sale-Is-Live_05.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/88c590d5-b492-424d-915b-f25451b1e32a1662101284472-Sale-Is-Live_06.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/db576ca3-e612-48b1-958a-07437f65f5181662101284479-Sale-Is-Live_07.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/54cd3a9c-6ca7-4175-8c72-f49dcfe4ab901662101284486-Sale-Is-Live_08.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/c1140912-a100-4d2c-a921-8e0324b09fd21662101284493-Sale-Is-Live_09.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/ed1f8e3c-7081-4ff5-9ffa-79f5a8c88ea21662101284500-Sale-Is-Live_10.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/6905f5cc-2f31-4652-94c5-b9f47308b8301662101284507-Sale-Is-Live_11.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/2bbec648-bbaa-4e74-9956-021b417424541662101284515-Sale-Is-Live_12.jpg`,
+    `https://assets.myntassets.com/f_webp,w_245,c_limit,fl_progressive,dpr_2.0/assets/images/2022/9/2/9ef3c5ab-3e4a-4eee-a13c-e607ff2db25b1662101284521-Sale-Is-Live_13.jpg`
+];
+
+let imagesRow4 = 
+[`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/c6eaa602-319a-45a2-921b-488b80a937841647420838828-Zaveri_Pearls-_Ami.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/d62aa762-63de-4696-b4b5-2d4d6356b80d1647420837964-H-M-_Max_and_more.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/261f62aa-02ff-47d6-87fd-020bebd79d6e1647420837738-All.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/5b28186c-2106-49c6-8aa4-ae0cd8ee31fb1647420837892-DressBerry_-_Clovia.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/1561dc8e-065d-4428-a72c-29ea8b85685a1647420838071-JJ-_FM.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/690c6112-6445-45ae-b0d8-56d370af72a91647420838815-Wrogn-_Single.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/b5a96633-505a-45a9-bdf3-0887250b82691647420838671-Tommy-_CK.png`];
+
+let imagesRow6 = [`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/45340b5b-0ce7-43c8-abc1-c5fb5571ba301647410586489-Budgetbuys_SportsShoes_.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/60a6a31c-1633-46d2-bd7f-d3a836f8424a1647410586226-Budgetbuys_Casual-Shoes_.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/2d86f892-d94c-471a-83a3-20265cd407101647410586071-Budgetbuys_-ActivewearKids--1-.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/7ea947af-0406-4b0e-b1ca-c5845f0980e81647410586079-Budgetbuys_-ActivewearMens.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/8e140318-81a0-4d05-afde-3f30d0dbadc61647410586302-Budgetbuys_Handbags.jpg`, 
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/de56f596-a7bd-4d80-998e-b6b040933ba91647410586579-Budgetbuys_T-Shirts--2.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/16/a04787ff-48bf-45ef-8524-d70d94d559641647410586270-Budgetbuys_Flip-Flops_.jpg`];
+
+let imagesRow8 = [`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/99a05311-2f73-4ea5-a25c-bdc40bbdb00b1657024220192-Libas_HP-Top-Tile.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/93c69d74-c456-49a2-a242-6711047330f21657024220200-Vishudh__HP-Top-Tile.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/ba04ecfe-6b75-46ed-9a02-87dad59d08071657010547010-TT-Top.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/0f2d7de3-5989-49e3-bc93-6de69dabf8221657028857324-Biotique-HP-Tile.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/8e27a543-cdab-428e-b3f4-a35f328ffa6b1657023441705-loreal-7th.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/4/7368c532-0aec-4159-8358-0ea985c9512e1656953708980-4.jpeg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/98f7304d-024f-408b-9e6f-9708615241991657006614596-_1896574772-2F_693850054-2F0bcd9db3_55e5_4666_a7a2_15dc6a827f1c-2Fimage_jpeg25750269.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/f831dac5-68a2-4329-883f-2b3326560e641657025556772-MATRIX.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/c7265cd6-587c-4e78-82fd-fb96879624721657023715787-puma-tile.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/3f8c4178-a958-47e7-a714-9e21b7917e331656995582794-H-M-tiles-1.jpg`,
+ `https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/0182c1c0-b5d8-4064-bbf5-f456df0062c91657012373972-image_jpeg_580506278.jpg`,
+ `https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/6/77e44ed6-718f-46f8-8060-f2017afc93de1657085562382-Van_HP-Tile_UD_July-7.jpg`,
+ `https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/e8808cca-088f-477a-a44c-64bda47b6e381657010748584-Caprese_WP-Tile_July-8.jpg`,
+ `https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/6/40c59e98-1b12-470d-97db-9aa05329354a1657084958187-image_jpeg1508835208.jpg`
+]
+
+let imagesRow10 = [
+    `https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/3/e31caf76-2649-4ea8-a39d-3ebc5006217e1656832965147-Vero_Moda.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/6/3998444e-8885-4290-9510-29cbd1c0c4791657087975656-unnamed---2022-07-06T114249.227.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/3/e132e58a-c705-46fb-9305-c35d9097a6141656832965118-Forever21.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/23/d461bb3b-29ba-4f7f-b715-750b87ca5b1e1648015528067-biba.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/3/a8045925-aebb-4f49-b4a1-9bd114f21f551656832965126-H-M.png`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/23/1723322c-0d0d-4645-94a0-ae5a7c56f91f1648015566914-pantaloons.jpg`,
+`https://assets.myntassets.com/w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/6/37ee4dca-50eb-48c5-9279-a57abba482c21657088000075-unnamed--24-.png`
+
+]
+
+let imagesRow12 = [
+    `https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/8d164aa4-a626-4983-bf1a-a8905a6037131657006133591-Home_DK_02.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/5b773418-02ca-48e4-b768-60433817e3461657006133598-Home_DK_03.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/fbe1d7e6-17f3-4606-9815-f03e86a4c3a11657006133604-Home_DK_04.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/477e42b0-dd3e-4cf6-9064-e8cc08c3217f1657006133611-Home_DK_05.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/76542ca3-b8de-4e14-b27c-f26bbe6040a21657006133617-Home_DK_06.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/aaf6dccc-94f5-4d85-ab01-d054fe12bb491657006133624-Home_DK_07.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/14b44b3e-a246-4675-84a1-f3015ffecf031657006133631-Home_DK_08.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/81d6ccac-d7df-4a6c-858b-ce573b2467d61657006133637-Home_DK_09.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/fe6761d1-10d3-4c36-87d2-6e54866159d21657006133645-Home_DK_10.jpg`,
+`https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/5b6bf2a5-4c35-4e7b-999f-4e4811a7c9d61657006133653-Home_DK_11.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/bc317610-b651-4e9e-99d0-4d260cb5b45d1657006133660-Home_DK_12.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/0926a6b0-d06a-4dd6-9726-337ff618ec061657006133669-Home_DK_13.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/54739f2e-d0ad-42b6-a731-883b31e204601657006133678-Home_DK_14.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/615d5eab-071d-44d1-afaa-4ea75dcf93cf1657006133685-Home_DK_15.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/a5b6143c-0848-405a-b11e-15bbb0c7daf81657006133692-Home_DK_16.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/9204598f-bdec-4a37-acd6-840457a7dc271657006133699-Home_DK_17.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/942528c6-5b7f-473e-b49c-eecc12d2715b1657006133706-Home_DK_18.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/2cc255f1-469d-4b3c-9f47-47488affa0831657006133713-Home_DK_19.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/4ff094cf-db60-4edc-8547-5a55738542691657006133719-Home_DK_20.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/4098e91c-30ab-49c7-853e-2f8da23473f51657006133727-Home_DK_21.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/eaef3d05-0b69-4ca6-9592-11fca0d4c4b71657006133734-Home_DK_22.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/efb6ef7f-e581-479d-928f-e4f5f9acf01f1657006133742-Home_DK_23.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/5717fc1d-d9b3-455b-ab2b-a5014c5564b21657006133749-Home_DK_24.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/a51c9b07-edf5-4dd2-a484-474fa00715351657006133756-Home_DK_25.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/7c7a2fe4-9757-4475-a334-938229f8c3a01657006133763-Home_DK_26.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/47c6e1ca-f79d-44f4-95bf-6e86d7df1b361657006133771-Home_DK_27.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/4ccf1baf-504f-4555-b7fe-ea3088bcda4a1657006133779-Home_DK_28.jpg`, 
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/355b997c-a2eb-418c-aeb0-a271d68a803e1657006133786-Home_DK_29.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/6bf2764c-6d04-4d7e-9870-e4dd5a400d9c1657006133795-Home_DK_30.jpg`,
+`https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/efc85dbb-6fb5-40f0-a62b-ec7e28837e561657006133804-Home_DK_31.jpg`
+]
+
+let img12Remaining2 = [
+    `https://assets.myntassets.com/w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/cd74be99-5ffb-428d-a616-2faffb1c16a21657006133812-Home_DK_32.jpg`,
+    `https://assets.myntassets.com/w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/5/f1a1ae0b-bb2d-4891-93b1-1f1ecd887b941657006133821-Home_DK_33.jpg`]
+
+let firstRowContainer = document.querySelector('.firstRowContainer');
+let fourthRowContainer = document.querySelector('.fourthRowContainer');
+let sixthRowContainer = document.querySelector('.sixthRowContainer');
+let eigthRowContainer = document.querySelector('.eigthRowContainer');
+let tenthRowContainer = document.querySelector('.tenthRowContainer');
+let twelthRowContainer = document.querySelector('.twelthRowContainer');
+let remaining2Container = document.querySelector('.remaining2Container');
+
+function addImagesToContainer(linksArray, container){
+    let imagesRowId = 0;
+  return linksArray.forEach((link)=>{
+    imagesRowId = imagesRowId + 1;
+        let anchorTag = document.createElement('a');
+        let imageTag = document.createElement('img');
+        anchorTag.append(imageTag);
+        anchorTag.href = 'addItem.html';
+        anchorTag.id = 'AnchorTagOf' + container.getAttribute('class') + imagesRowId + 'item';
+        imageTag.id = container.getAttribute('class') + imagesRowId + 'item';
+        imageTag.src = link;
+        container.appendChild(anchorTag);
+    })
+}
+
+
+window.addEventListener('load', addImagesToContainer(imagesRow1, firstRowContainer));
+window.addEventListener('load', addImagesToContainer(imagesRow4, fourthRowContainer));
+window.addEventListener('load', addImagesToContainer(imagesRow6, sixthRowContainer));
+window.addEventListener('load', addImagesToContainer(imagesRow8, eigthRowContainer));
+window.addEventListener('load', addImagesToContainer(imagesRow10, tenthRowContainer));
+window.addEventListener('load', addImagesToContainer(imagesRow12, twelthRowContainer));
+window.addEventListener('load', addImagesToContainer(img12Remaining2, remaining2Container));
+
+// After Header Part of the Body ENDS Here:-
+
+
+
+
+// footer starts here​⁡⁡​
+
+
+let popularSearchesKeywords = document.querySelector('.popularSearchesKeywords');
+
+let keywords = ['Makeup | ', 'Dresses For Girls | ', 'T-Shirts | ', 'Sandals | ', 'Headphones | ', 'Babydolls | ', 'Blazers For Men | ', 'Handbags | ', 'Ladies Watches | ', 'Bags | ', 'Sport Shoes | ', 'Reebok Shoes | ', 'Puma Shoes | ', 'Boxers | ', 'Wallets | ', 'Tops | ', 'Earrings | ', 'Fastrack Watches | ', 'Kurtis | ', 'Nike | ', 'Smart Watches | ', 'Titan Watches | ', 'Designer Blouse | ', 'Gowns | ', 'Rings | ', 'Cricket Shoes | ', 'Forever 21 | ', 'Eye Makeup | ', 'Photo Frames | ', 'Punjabi Suits | ', 'Bikini | ', 'Myntra Fashion Show | ', 'Lipstick | ', 'Saree | ', 'Watches | ', 'Dresses | ', 'Lehenga | ', 'Nike Shoes | ', 'Goggles | ', 'Bras | ', 'Suit | ', 'Chinos | ', 'Shoes | ', 'Adidas Shoes | ', 'Woodland Shoes | ', 'Jewellery | ', 'Designers Sarees'];
+
+keywords.forEach(function(keyword) {
+    let newAnchor = document.createElement('a');
+    newAnchor.innerText = keyword;
+    newAnchor.href = '#';
+    newAnchor.classList.add('normalFooterText');
+    popularSearchesKeywords.appendChild(newAnchor);
+
+});
+
+
+// Testing category products page
+
+let AnchorTagOffourthRowContainer2item = document.getElementById('AnchorTagOffourthRowContainer2item');
+
+AnchorTagOffourthRowContainer2item.href = 'addItem.html';
+
+
+
+
+
+
+
+
+
+
